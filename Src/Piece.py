744f692b62e0d6f3a531559ea21f55c767230a92
kevin <kevinfritsch@hotmail.fr>
2020-05-07 11:57:02 +0200

    Modifs erreurs

--- Src/Piece.py
+++ Src/Piece.py
@@ -6,10 +6,10 @@ class Piece:
         self.position=[None,None]
         self.image = pygame.image.load(_image.value[0])
 
-        #Possible erreur: pygame.Surface ne connait pas 'Rezise'
+        #erreur: 'Rezise' ?
         #self.image.Rezise = pygame.transform.scale(self.image,_image.value[1]) 
         #self.positionImage =self.image.Rezise.get_rect()
 
         #Solution
-        self.image = pygame.transform.scale(self.image,_image.value[1]) 
-        self.positionImage =self.image.get_rect()
+        self.imageRezise = pygame.transform.scale(self.image,_image.value[1]) 
+        self.positionImage =self.imageRezise.get_rect()
